808515855372268e8323afcff8240d9459fc892a
achursin1986 <achursin1986@gmail.com>
2022-07-07 12:37:58 +0200

    homework 2 initial commit

---
+++ 2/move_me_out.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "move_me_out.h"
+
+void todo_move_me_out() {
+        std::cout << "This function is located in the separated source file" << std::endl;
+}
---
+++ 2/move_me_out.h
@@ -0,0 +1,5 @@
+# pragma once
+
+void todo_move_me_out();
+
+
