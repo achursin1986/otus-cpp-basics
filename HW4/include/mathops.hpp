92d72cfb2940b847ac4bc0a261216138aa44f566
achursin1986 <achursin1986@gmail.com>
2022-08-05 02:02:27 +0200

    HW4 basic task

---
+++ HW4/include/mathops.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+
+#include "astnode.hpp"
+
+
+// Addition
+
+class Add : public ASTNode {
+  public:
+    Add(ASTNode* left, ASTNode* right)
+        : ASTNode("+", left, right ) {}
+
+};
+
+// Subsctraction
+
+class Sub : public ASTNode {
+  public:
+    Sub(ASTNode* left, ASTNode* right)
+        : ASTNode("-", left, right )  {}
+
+};
+
+// Multiplication
+
+class Mul : public ASTNode {
+  public:
+    Mul(ASTNode* left, ASTNode* right)
+        : ASTNode("*", left, right ) {}
+};
+
+
+// Division
+
+class Div : public ASTNode {
+  public:
+    Div(ASTNode* left, ASTNode* right)
+        : ASTNode(":", left, right) {}
+};
+
